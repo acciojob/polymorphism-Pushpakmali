a7ad86c2a7257448360b5b78a3f1cd30e9efcfb4
Pushpak <pushpakmali44@gmail.com>
2023-05-16 18:44:29 +0530

    All changes done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+        public Product(){
+        }
 
+        public int product(int x, int y){
+            return x*y;
+        }
+
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
+
+    public static void main(String args[]){
+        Product p = new Product();
+        int ans1 = p.product(2, 5);
+        int ans2 = p.product(3,5,7);
+        double ans3 = p.product(2.56, 5.48);
+    }
 }
